7256f9e459ddda722e78fa81c819223450afdcff
Luiz Pereira <lfhbpereira@gmail.com>
2022-09-09 15:12:37 -0300

    feat: Add requirement 3 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import PageGame from './pages/PageGame';
+import Settings from './pages/Settings';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route path="/game" component={ PageGame } />
+          <Route path="/settings" component={ Settings } />
         </Switch>
       </div>
     );
